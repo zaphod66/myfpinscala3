243853d4e66a4e5cb14cf6a54e6519ebd1a4f70a
Norbert Scheller <norbert@riskident.com>
2021-10-04 18:15:03 +0200

    ch06 - started

---
+++ src/main/scala/ch06/State.scala
@@ -0,0 +1,17 @@
+package ch06
+
+trait RNG:
+  def nextInt: (Int, RNG)
+
+object RNG:
+  case class Simple(seed: Long) extends RNG:
+    def nextInt: (Int, RNG) =
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n       = (newSeed >>> 16).toInt
+
+      (n, nextRNG)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) =
+    val (i, r) = rng.nextInt
+    (if i < 0 then -(i + 1) else i, r)
